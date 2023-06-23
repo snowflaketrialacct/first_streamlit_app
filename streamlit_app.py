59a3c329962cac45f7dd725423a742b129bad864
snowflaketrialacct <136729690+snowflaketrialacct@users.noreply.github.com>
2023-06-23 14:09:08 -0500

    Update streamlit_app.py
    stopping streamlit

--- streamlit_app.py
+++ streamlit_app.py
@@ -56,6 +56,9 @@ if streamlit.button('Get Fruit Load List'):
 	my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 	my_data_rows = get_fruit_load_list()
 	streamlit.dataframe(my_data_rows)
+
+# Don't run anything until troubleshooting is done
+streamlit.stop()
 		
 # Allow end user to add their own fruit
 add_my_fruit = streamlit.text_input('What fruit would you like to add?')
